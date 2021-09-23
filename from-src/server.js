6b33bda167be3805eb21128d4bc3631486e5de55
luisreyesoliva <luis.reyes@es.ibm.com>
2021-09-23 11:35:22 +0200

    Update server.js

--- from-src/server.js
+++ from-src/server.js
@@ -10,7 +10,7 @@ app.get('/', (request, response) => {
     <title>Powered By Code Engine</title>
   </head>
   <body>
-    Hola asistentes al Cloud & DevOps days!!
+    Hola mundo!!
   </body>
 </html>`);
 });
